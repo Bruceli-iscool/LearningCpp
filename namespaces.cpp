ec9dcd5ac422ed20f8052524ef68a5f95608918d
Bruce Li <isanoob1897@outlook.com>
2024-01-27 11:16:06 -0800

    add and learn

--- namespaces.cpp
+++ namespaces.cpp
@@ -7,6 +7,12 @@ namespace first{
 int main() {
     // reclare x
     int x = 0;
+    // oop aplies when we print x
+    std::cout << x;
+    // returns 0 the local x
+    // print first x
+    std::cout << first::x;
+    return 0;
     
 
 }
