cf1709294decfc9ca887b4f56c274e170d66db24
Bruce Li <isanoob1897@outlook.com>
2024-01-26 21:49:02 -0800

    add

---
+++ variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x; // declare variable as a whole integer
+    x = 5; // assign value to variable
+    int age = 11;
+    // declare a double
+    double price = 1.2; // with a decimal
+    cout << x + age + price;
+    return 0;
+}
