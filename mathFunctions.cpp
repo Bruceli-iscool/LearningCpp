1082f33668de8b05dc21bfa3e6a85d8a6b43d0b4
Bruce Li <isanoob1897@outlook.com>
2024-01-29 19:10:41 -0800

    add

--- mathFunctions.cpp
+++ mathFunctions.cpp
@@ -6,6 +6,17 @@ int main() {
     double z;
     // max function returns largest num
     z = std::max(x, y);
-    std::cout << z;
+    std::cout << z << "\n";
+    // min function returns smallest num
+    double yx;
+    yx = std::min(x, y);
+    std::cout << yx << "\n";
+    // power
+    z = std::pow(2, 3);
+    std::cout << z << "\n";
+    // returns 8
+    // square root
+    z = std::sqrt(5);
+    std::cout << z << "\n";
     return 0;
 }
